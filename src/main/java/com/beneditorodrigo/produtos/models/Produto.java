92adcac1ab2378a85b6835edf7cf2e9bee7ae671
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-08-20 16:45:10 -0300

    Criando models/entidade Produto

---
+++ src/main/java/com/beneditorodrigo/produtos/models/Produto.java
@@ -0,0 +1,49 @@
+package com.beneditorodrigo.produtos.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	private String nome;
+
+	private BigDecimal quantidade;
+
+	private BigDecimal valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+}
