89ddea836dcb075b029f399656d81af99b3d3f72
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-08-20 22:06:18 -0300

    Adcionando anotações na entidade Produto

--- src/main/java/com/beneditorodrigo/produtos/models/Produto.java
+++ src/main/java/com/beneditorodrigo/produtos/models/Produto.java
@@ -3,10 +3,20 @@ package com.beneditorodrigo.produtos.models;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO")
 public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String nome;
